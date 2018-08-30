95495999669cf510e9bd1cf188f66f3bb401f439
Ankit Modi <ankitmodi31198@gmail.com>
2018-08-30 22:49:45 +0530

    Machine Learning Andrew NG course Answers week 2

--- machine-learning-ex1/ex1/plotData.m
+++ machine-learning-ex1/ex1/plotData.m
@@ -23,7 +23,8 @@ x = data(:, 1);
 y = data(:, 2);
 m = length(y);
 
-plot(x, y, 'rx', 'MarkerSize', 10);
+plot(x, y, 'rx', 'MarkerSize', 10);\
+
 xlabel('Population of City in 10,000s');
 ylabel('Profit in $10,000s');
 
